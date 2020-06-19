b33554e6deb7cfa436de80bf19c55cf631a617a0
iqbaleffendy <miqbaleffendy18@gmail.com>
2020-06-19 22:02:21 +0700

    Removing empty space between lines

--- ServicePerformance.R
+++ ServicePerformance.R
@@ -1,37 +1,26 @@
 library(readxl)
-
 setwd("D:\\Altrak 1978\\Service Agri\\Data Service Agri\\Test R")
-
 mydata <- read_excel("jobrptmo.xlsx")
 
 
 #giving column names
-
 colnames(mydata) <- c("JobNo", "OpenDate", "CloseDate", "Customer", "JobDesc", "Parts", 
                       "Labour", "Others")
 
 
 #adding job valuation
-
 mydata$Service <- mydata$Labour + mydata$Others
-
 mydata$TotalValue <- mydata$Service + mydata$Parts
 
 
 #calculating open and closing date
-
 library(lubridate)
-
 library(dplyr)
-
 mydata$OpenDate <- as.Date(mydata$OpenDate)
-
 mydata$CloseDate <- as.Date(mydata$CloseDate)
-
 mydata$TotalDays <- ifelse(is.na(mydata$CloseDate), 
                             difftime(Sys.Date(), mydata$OpenDate, units = "days"),
                             difftime(mydata$CloseDate, mydata$OpenDate, units = "days"))
-
 mydata$OpenYear <- format(mydata$OpenDate, "%Y")
 
 
@@ -58,66 +47,37 @@ mydata <- mutate(mydata, OverhaulJob = case_when(
 #determine job type and branch code
 
 mydata$JobType <- substr(mydata$JobNo, 1,2)
-
 mydata$BranchCode <- substr(mydata$JobNo, 13,14)
-
 mydata$Agency <- substr(mydata$JobNo, 9,11)
 
 
-
 #lookup value for job category
-
 jobcategory <- read_excel("Job Category.xlsx")
-
 colnames(jobcategory) <- c("JobType", "JobCategory")
-
 mydata <- merge(mydata, jobcategory, by="JobType")
-
 mydata$JobCategory <- substr(mydata$JobCategory, 1, 12)
 
-
 #lookup value for branch code
-
 branchcode <- read_excel("Branch Code.xlsx")
-
 colnames(branchcode) <- c("BranchCode", "BranchName")
-
 mydata <- merge(mydata, branchcode, by="BranchCode")
 
-
 #rearranging the order of columns
-
 head(mydata)
-
 finaldata <- mydata[ , c("JobNo", "Agency", "JobType", "JobCategory", "BranchCode", "BranchName",
                          "OpenYear", "OpenDate", "CloseDate", "TotalDays", "JobStatus",
                          "OutstandingJobClass", "Customer", "JobDesc", "OverhaulJob", "Parts", "Labour", 
                          "Others", "Service", "TotalValue")]
 
 finaldata$JobType <- factor(finaldata$JobType)
-
 finaldata$Agency <- factor(finaldata$Agency)
-
 finaldata$JobCategory <- factor(finaldata$JobCategory)
-
 finaldata$BranchCode <- factor(finaldata$BranchCode)
-
 finaldata$BranchName <- factor(finaldata$BranchName)
-
 finaldata$OpenYear <- factor(finaldata$OpenYear)
-
 finaldata$JobStatus <- factor(finaldata$JobStatus)
-
-str(finaldata)
 
 
 #export final data
-
 library(xlsx)
-
-getwd()
-
 write.xlsx(finaldata, "serviceperformance.xlsx")
-
-
-
