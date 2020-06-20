feca5ed4b0d789112f33a2d1b1f8e771b67010f8
iqbaleffendy <miqbaleffendy18@gmail.com>
2020-06-20 08:32:56 +0700

    Adding layout for 'Performance' tab, and interactive select input on the sidebar

--- serviceAFdashboard.R
+++ serviceAFdashboard.R
@@ -1,17 +1,52 @@
+library(shiny)
 library(shinydashboard)
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(DT)
+library(plotly)
+
+
+# Load dataset
+
+mydata <- read_excel("serviceperformance.xlsx")
+
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Service A&F Dashboard"),
+  
+  # Dashboard Header
+  dashboardHeader(title = "Service A&F"),
+  
+  # Dashboard Sidebar
   dashboardSidebar(
     sidebarMenu(
       menuItem("Performance", tabName = "performance"),
       menuItem("Population", tabName = "population"),
-      menuItem("Failure Analysis", tabName = "analysis")
+      menuItem("Failure Analysis", tabName = "analysis"),
+      selectInput("agency", "Select Agency", choices = unique(mydata$Agency)),
+      selectInput("branchname", "Select Branch", choices = unique(mydata$BranchName))
     )
   ),
+  
+  # Dashboard Body
   dashboardBody(
     tabItems(
-      tabItem("performance"),
+      tabItem("performance",
+        fluidRow(
+          valueBox(value = 10, subtitle = "servicevalue"),
+          valueBox(value = 10, subtitle = "partsvalue"),
+          valueBox(value = 10, subtitle = "outstandingjob")
+        ),
+        fluidRow(
+          box(title = "Filter",
+              solidHeader = TRUE,
+              width = 4,
+              collapsible = TRUE),
+          box(title = "Chart",
+              solidHeader = TRUE,
+              width = 8)
+        )
+      ),
       tabItem("population"),
       tabItem("analysis")
     )
