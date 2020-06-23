f713db624203b52ad337ad584491734f42100c57
iqbaleffendy <miqbaleffendy18@gmail.com>
2020-06-23 14:43:09 +0700

    Adding interactive select input to choose agency or branch, and create bar chart, pie chart, value boxes that corresponds to inputs in the server side.

--- serviceAFdashboard.R
+++ serviceAFdashboard.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinydashboard)
+library(shinyWidgets)
 library(readxl)
 library(tidyverse)
 library(ggplot2)
@@ -7,7 +8,7 @@ library(DT)
 library(plotly)
 
 
-# Load dataset
+# Load Dataset
 
 mydata <- read_excel("serviceperformance.xlsx")
 
@@ -21,10 +22,20 @@ ui <- dashboardPage(
   dashboardSidebar(
     sidebarMenu(
       menuItem("Performance", tabName = "performance"),
-      menuItem("Population", tabName = "population"),
+      menuItem("Dataset", tabName = "dataset"),
       menuItem("Failure Analysis", tabName = "analysis"),
-      selectInput("agency", "Select Agency", choices = unique(mydata$Agency)),
-      selectInput("branchname", "Select Branch", choices = unique(mydata$BranchName))
+      selectInput(
+        inputId = "agency", 
+        label = "Select Agency",
+        choices = c("All", unique(mydata$Agency)),
+        selected = "All"
+      ),
+      selectInput(
+        inputId = "branchname",
+        label = "Select Branch",
+        choices = c("All", unique(mydata$BranchName)),
+        selected = "All"
+      )
     )
   ),
   
@@ -33,27 +44,92 @@ ui <- dashboardPage(
     tabItems(
       tabItem("performance",
         fluidRow(
-          valueBox(value = 10, subtitle = "servicevalue"),
-          valueBox(value = 10, subtitle = "partsvalue"),
-          valueBox(value = 10, subtitle = "outstandingjob")
+          valueBoxOutput("servicevalue"),
+          valueBoxOutput("partsvalue"),
+          valueBoxOutput("warrantyvalue")
         ),
         fluidRow(
-          box(title = "Filter",
+          box(title = "Job Type Percentage",
               solidHeader = TRUE,
               width = 4,
-              collapsible = TRUE),
-          box(title = "Chart",
+              plotOutput("piechart")),
+          box(title = "Service Performance",
               solidHeader = TRUE,
-              width = 8)
+              width = 8,
+              plotOutput("barchart"))
         )
       ),
-      tabItem("population"),
+      tabItem("dataset",
+        DTOutput("table")
+      ),
       tabItem("analysis")
     )
   )
 )
 
 server <- function(input, output, session) {
+  
+  mydata_filtered <- reactive({
+    if (input$agency != "All") {
+      mydata <- mydata %>% 
+        filter(Agency == input$agency)
+    }
+  
+    if (input$branchname != "All") {
+      mydata <- mydata %>% 
+        filter(BranchName == input$branchname)
+    }
+    mydata
+  })
+  
+  output$barchart <- renderPlot({
+    if (input$branchname != "All") {
+      mydata_filtered() %>% 
+        ggplot(aes(x = fct_infreq(JobType))) +
+        geom_bar()
+    } else {
+      mydata_filtered() %>% 
+        ggplot(aes(x = fct_infreq(BranchName), fill = JobCategory)) +
+        geom_bar()
+    }
+  })
+  
+  output$piechart <- renderPlot({
+    mydata_filtered() %>%
+      count(JobType) %>% 
+      ggplot(aes(x = "", y = n, fill = JobType)) +
+      geom_bar(stat = "identity") +
+      coord_polar(theta = "y", start = 0)
+  })
+  
+  output$servicevalue <- renderValueBox({
+    valueBox(
+      mydata_filtered() %>% 
+        summarize(sum(Service)),
+      "Service Value"
+    )
+  })
+  
+  output$partsvalue <- renderValueBox({
+    valueBox(
+      mydata_filtered() %>% 
+        summarize(sum(Parts)),
+      "Parts Value"
+    )
+  })
+  
+  output$warrantyvalue <- renderValueBox({
+    valueBox(
+      mydata_filtered() %>% 
+        filter(JobType == "IW") %>% 
+        summarize(sum(TotalValue)),
+      "Warranty Value"
+    )
+  })
+  
+  output$table <- renderDT({
+    mydata_filtered()
+  })
   
 }
 
